50359b5ac1d844ae407a7c0f08e014e4a0ed89d8
aland1013 <adam.land@gmail.com>
2021-03-30 07:35:41 -0400

    solved 617 merge binary trees

---
+++ 617-merge-binary-trees.js
@@ -0,0 +1,66 @@
+/*
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+Example 2:
+
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 2000].
+-104 <= Node.val <= 104
+*/
+
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+const mergeTrees = (root1, root2) => {
+  let root = new TreeNode();
+
+  if (root1 && root2) {
+    root.val = root1.val + root2.val;
+
+    root.left = mergeTrees(root1.left, root2.left);
+    root.right = mergeTrees(root1.right, root2.right);
+  }
+
+  if (root1 && !root2) {
+    root.val = root1.val;
+
+    root.left = mergeTrees(root1.left, null);
+    root.right = mergeTrees(root1.right, null);
+  }
+
+  if (!root1 && root2) {
+    root.val = root2.val;
+
+    root.left = mergeTrees(root2.left, null);
+    root.right = mergeTrees(root2.right, null);
+  }
+
+  if (!root1 && !root2) {
+    root = null;
+  }
+
+  return root;
+};
